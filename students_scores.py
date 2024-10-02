61954fe33a17f8868400d2ab4a13de5c10d7af28
NikaKhiz <khizambarelinika123@gmail.com>
2024-10-02 08:52:57 +0400

    display students who have failed

--- students_scores.py
+++ students_scores.py
@@ -19,11 +19,32 @@ class StudentScores:
         self.data.fillna(0, inplace=True)
 
 
+    def students_who_failed(self):
+        # Return students who failed in any subject
+        condition = (self.data[self.subjects] < 50).any(axis=1)
+        failed_students = self.data[condition]
+        return failed_students.drop_duplicates(subset='Student')
+
+
+    def analyze(self):
+        # Analyze and store values in properties 
+
+        self.failed_students = self.students_who_failed()
+
+
+    def display_analyzed_data(self):
+        # Print the results
+
+        print('Students who didn\'t pass exams:\n', self.failed_students)
+        print('-------------------------------')
+
 
 def main():
     SUBJECTS = ['Math', 'Physics', 'Chemistry', 'Biology', 'English']
     analyzer = StudentScores('student_scores_random_names.csv', SUBJECTS)
-    
+    analyzer.analyze() 
+    analyzer.display_analyzed_data() 
+
 
 if __name__ == '__main__':
     main()
